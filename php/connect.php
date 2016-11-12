d8384b95962980bd4023eef072962475bf7a1b25
dale <dalewang1995@gmail.com>
2016-11-12 22:52:18 +0800

    查询学生信息
    输入 学号 姓名 学生基本信息

---
+++ php/connect.php
@@ -0,0 +1,19 @@
+<?php
+$server="localhost";//主机
+$db_username="root";//数据库用户名
+$db_password="";//数据库密码
+$con = mysqli_connect($server,$db_username,$db_password);//链接数据库
+if(!$con){
+    die("can't connect".mysqli_error());//如果链接失败输出错误
+}
+$result = mysqli_select_db('test',$con);//选择数据库
+var_dump($result);
+//$query = 'SELECT * FROM t_grade WHERE 学号=20144754';
+//
+//$result = mysqli_query($query) or die('Query failed: ' . mysqli_error());
+
+
+// 用 HTML 显示结果
+
+//echo  $result;
+?>
---
+++ php/query_grade.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// 执行 SQL 查询
+
+$query = 'SELECT * FROM t_grade WHERE 学号=20144754';
+
+$result = mysqli_query($query) or die('Query failed: ' . mysqli_error());
+
+
+// 用 HTML 显示结果
+
+echo  $result;
+
+?>
