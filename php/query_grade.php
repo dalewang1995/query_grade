4e92e3e7fb37db86e0cb6bda5623d1da47ff3198
coderw <dalewang1995@gmail.com>
2019-08-10 23:03:13 +0800

    Update query_grade.php

--- php/query_grade.php
+++ php/query_grade.php
@@ -3,7 +3,7 @@
 
 // 执行 SQL 查询
 
-$query = 'SELECT * FROM t_grade WHERE 学号=20144754';
+$query = 'SELECT * FROM t_grade WHERE 学号=xxx';
 
 $result = mysqli_query($query) or die('Query failed: ' . mysqli_error());
 
